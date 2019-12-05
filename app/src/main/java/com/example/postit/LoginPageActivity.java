7a3466ec9bb74813ea8f48d7fb2fd86e38586f23
AnAsianGangster <AnAsianGangster@users.noreply.github.com>
2019-12-05 19:43:56 +0800

    save

--- app/src/main/java/com/example/postit/LoginPageActivity.java
+++ app/src/main/java/com/example/postit/LoginPageActivity.java
@@ -5,18 +5,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.postit.eventlisting.ViewEventsActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginPageActivity extends AppCompatActivity {
 
-    Button loginBtn;
+    private EditText emailAddress, password;
+    private Button loginBtn;
     Button signUpBtn;
-    EditText emailAddress;
-    EditText password;
+    // TODO change the URL to our url
+//    private static String URL_LOGIN = "http://10.12.115.166/newPHP/login.php";
+    private static String URL_LOGIN = "http://13.67.95.12/auth/login";
+    // http://13.67.95.12/auth/login
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,13 +46,74 @@ public class LoginPageActivity extends AppCompatActivity {
         emailAddress = findViewById(R.id.emailAddress);
         password = findViewById(R.id.password);
 
-    signUpBtn.setOnClickListener(new View.OnClickListener() {
+        // TODO delete the hardcode for testing
+        emailAddress.setText("mihir");
+        password.setText("skydiving");
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mUsername = emailAddress.getText().toString().trim();
+                String mPassword = password.getText().toString().trim();
+
+
+                if(!mUsername.isEmpty() || !mPassword.isEmpty()){
+                    Login(mUsername, mPassword);
+                } else {
+                    emailAddress.setError("Please insert email");
+                    password.setError("Please insert password");
+
+                }
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(getApplicationContext(), RegistrationPageActivity.class);
             startActivity(intent);
+
         }
     });
 
+    }
+
+    private void Login(final String username, final String password) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String sucess = jsonObject.getString("status");
+                            Toast.makeText(LoginPageActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(LoginPageActivity.this, ViewEventsActivity.class);
+                            startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(LoginPageActivity.this, "Error "+e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginPageActivity.this, "Error "+error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
---
+++ app/src/main/java/com/example/postit/requests/UserLoginRequest.java
@@ -0,0 +1,8 @@
+package com.example.postit.requests;
+
+import com.example.postit.App;
+import com.example.postit.R;
+
+public class UserLoginRequest extends ActivityRequests {
+    private static final String loginHttp = "http://13.67.95.12/auth/login";
+}
